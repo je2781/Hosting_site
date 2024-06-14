dca44f45d9c63d5884236a546d0134d3b1bde981
Joshua Eze <118199550+je2781@users.noreply.github.com>
2024-06-14 12:17:54 +0100

    Update LatestNews.js

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -10,7 +10,7 @@ const LatestNews = () => {
                             </div>
                         </div>
                         <div className="col-md-6 col-12 d-flex justify-content-end">
-                            <a href="#" className="link hs-btn">View more</a>
+                            <a href="/" className="link hs-btn">View more</a>
                         </div>
                     </div>
                 </div>
@@ -19,56 +19,56 @@ const LatestNews = () => {
                         <div className="latest-news-box latest-news-1 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>Discover our new<br/>website builder</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-lg-4 d-none d-lg-block">
                         <div className="latest-news-box latest-news-2 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>New Data center in<br/>Germany & USA</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-lg-4 d-none d-lg-block">
                         <div className="latest-news-box latest-news-3 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>Discover our new<br/>website plugins</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-md-6 d-none d-md-block d-lg-none">
                         <div className="latest-news-box latest-news-1 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>Discover our new<br/>website builder</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-md-6 d-none d-md-block d-lg-none">
                         <div className="latest-news-box latest-news-2 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>New Data center in<br/>Germany & USA</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-12 d-block d-md-none">
                         <div className="latest-news-box latest-news-1 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>Discover our new website builder</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-12 d-block d-md-none">
                         <div className="latest-news-box latest-news-2 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>New Data center in Germany & USA</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                     <div className="col-12 d-block d-lg-none">
                         <div className="latest-news-box latest-news-3 d-flex flex-column justify-content-end">
                             <div className="latest-news-header">HOT</div>
                             <div className="latest-news-body"><strong>Discover our new website plugins</strong></div>
-                            <div className="latest-news-footer"><a href="#" className="link hs-btn">Learn more</a></div>
+                            <div className="latest-news-footer"><a href="/" className="link hs-btn">Learn more</a></div>
                         </div>
                     </div>
                 </div>
