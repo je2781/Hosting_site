5cce79b995eaebff47475cb70ab9125d4fbcb1d3
Joshua Eze <118199550+je2781@users.noreply.github.com>
2024-06-14 12:08:56 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return(
         <div id="footer" className="container-fluid">
             <div className="d-flex justify-content-center">
-                <a href="#" className="go-top-btn"><i style={{color: '#fff', fontSize: '26px'}} className="bi bi-chevron-up"></i></a>                   
+                <a href="/" className="go-top-btn"><i style={{color: '#fff', fontSize: '26px'}} className="bi bi-chevron-up"></i></a>                   
             </div>
             <div className="container">
                 <div className="row footer-body">
@@ -11,38 +11,38 @@ const Footer = () => {
                             <div className="col-md-3">
                                 <ul className="list-1">
                                     <li className="col-header">Company</li>
-                                    <li><a className="item" href="#">About us</a></li>
-                                    <li><a className="item" href="#">Press & Media</a></li>
-                                    <li><a className="item" href="#">News & Blog</a></li>
-                                    <li><a className="item" href="#">Contact Us</a></li>
-                                    <li><a className="item" href="#">Careers</a></li>
+                                    <li><a className="item" href="/">About us</a></li>
+                                    <li><a className="item" href="/">Press & Media</a></li>
+                                    <li><a className="item" href="/">News & Blog</a></li>
+                                    <li><a className="item" href="/">Contact Us</a></li>
+                                    <li><a className="item" href="/">Careers</a></li>
                                 </ul>
                             </div>
                             <div className="col-md-3">
                                 <ul className="list-2">
                                     <li className="col-header">Hosting</li>
-                                    <li><a className="item" href="#">Web Hosting</a></li>
-                                    <li><a className="item" href="#">WordPress Hosting</a></li>
-                                    <li><a className="item" href="#">Cloud Hosting</a></li>
-                                    <li><a className="item" href="#">VPS Hosting</a></li>
-                                    <li><a className="item" href="#">Dedicated Hosting</a></li>
+                                    <li><a className="item" href="/">Web Hosting</a></li>
+                                    <li><a className="item" href="/">WordPress Hosting</a></li>
+                                    <li><a className="item" href="/">Cloud Hosting</a></li>
+                                    <li><a className="item" href="/">VPS Hosting</a></li>
+                                    <li><a className="item" href="/">Dedicated Hosting</a></li>
                                 </ul>
                             </div>
                             <div className="col-md-3">
                                 <ul className="list-3">
                                     <li className="col-header">Domains</li>
-                                    <li><a className="item" href="#">Register Domains</a></li>
-                                    <li><a className="item" href="#">Transfer Domains</a></li>
-                                    <li><a className="item" href="#">Manage Domains</a></li>
+                                    <li><a className="item" href="/">Register Domains</a></li>
+                                    <li><a className="item" href="/">Transfer Domains</a></li>
+                                    <li><a className="item" href="/">Manage Domains</a></li>
                                 </ul>
                             </div>
                             <div className="col-md-3">
                                 <ul className="list-4">
                                     <li className="col-header">Support</li>
-                                    <li><a className="item" href="#">Client area</a></li>
-                                    <li><a className="item" href="#">Tutorials</a></li>
-                                    <li><a className="item" href="#">Documentation</a></li>
-                                    <li><a className="item" href="#">Forums</a></li>
+                                    <li><a className="item" href="/">Client area</a></li>
+                                    <li><a className="item" href="/">Tutorials</a></li>
+                                    <li><a className="item" href="/">Documentation</a></li>
+                                    <li><a className="item" href="/">Forums</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -52,21 +52,21 @@ const Footer = () => {
                             <div className="col-6 d-block d-md-none">
                                 <ul className="list-1">
                                     <li className="col-header">Company</li>
-                                    <li><a className="item" href="#">About us</a></li>
-                                    <li><a className="item" href="#">Press & Media</a></li>
-                                    <li><a className="item" href="#">News & Blog</a></li>
-                                    <li><a className="item" href="#">Contact Us</a></li>
-                                    <li><a className="item" href="#">Careers</a></li>
+                                    <li><a className="item" href="/">About us</a></li>
+                                    <li><a className="item" href="/">Press & Media</a></li>
+                                    <li><a className="item" href="/">News & Blog</a></li>
+                                    <li><a className="item" href="/">Contact Us</a></li>
+                                    <li><a className="item" href="/">Careers</a></li>
                                 </ul>
                             </div>
                             <div className="col-6 d-block d-md-none">
                                 <ul className="list-2">
                                     <li className="col-header">Hosting</li>
-                                    <li><a className="item" href="#">Web Hosting</a></li>
-                                    <li><a className="item" href="#">WordPress Hosting</a></li>
-                                    <li><a className="item" href="#">Cloud Hosting</a></li>
-                                    <li><a className="item" href="#">VPS Hosting</a></li>
-                                    <li><a className="item" href="#">Dedicated Hosting</a></li>
+                                    <li><a className="item" href="/">Web Hosting</a></li>
+                                    <li><a className="item" href="/">WordPress Hosting</a></li>
+                                    <li><a className="item" href="/">Cloud Hosting</a></li>
+                                    <li><a className="item" href="/">VPS Hosting</a></li>
+                                    <li><a className="item" href="/">Dedicated Hosting</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -76,18 +76,18 @@ const Footer = () => {
                             <div className="col-6 d-block d-md-none">
                                 <ul className="list-3">
                                     <li className="col-header">Domains</li>
-                                    <li><a className="item" href="#">Register Domains</a></li>
-                                    <li><a className="item" href="#">Transfer Domains</a></li>
-                                    <li><a className="item" href="#">Manage Domains</a></li>
+                                    <li><a className="item" href="/">Register Domains</a></li>
+                                    <li><a className="item" href="/">Transfer Domains</a></li>
+                                    <li><a className="item" href="/">Manage Domains</a></li>
                                 </ul>
                             </div>
                             <div className="col-6 d-block d-md-none">
                                 <ul className="list-4">
                                     <li className="col-header">Support</li>
-                                    <li><a className="item" href="#">Client area</a></li>
-                                    <li><a className="item" href="#">Tutorials</a></li>
-                                    <li><a className="item" href="#">Documentation</a></li>
-                                    <li><a className="item" href="#">Forums</a></li>
+                                    <li><a className="item" href="/">Client area</a></li>
+                                    <li><a className="item" href="/">Tutorials</a></li>
+                                    <li><a className="item" href="/">Documentation</a></li>
+                                    <li><a className="item" href="/">Forums</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -98,15 +98,15 @@ const Footer = () => {
                                 <ul className="list-5">
                                     <li className="col-header">Contact Us</li>
                                     <li>
-                                        <a className="brand-icon" href="#"><i className="bi bi-facebook"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-youtube"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-twitter"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-instagram"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-facebook"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-youtube"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-twitter"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-instagram"></i></a>
                                     </li>
                                     <li className="tel">Tel:&nbsp;+(973)&nbsp;17&nbsp;880038</li>
-                                    <li><a className="item" href="#">Chatting service</a></li>
-                                    <li><a className="item" href="#">Submit a Ticket</a></li>
-                                    <li><a className="item" href="#">Our location</a></li>
+                                    <li><a className="item" href="/">Chatting service</a></li>
+                                    <li><a className="item" href="/">Submit a Ticket</a></li>
+                                    <li><a className="item" href="/">Our location</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -117,15 +117,15 @@ const Footer = () => {
                                 <ul className="list-5">
                                     <li className="col-header">Contact Us</li>
                                     <li>
-                                        <a className="brand-icon" href="#"><i className="bi bi-facebook"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-youtube"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-twitter"></i></a>
-                                        <a className="brand-icon" href="#"><i className="bi bi-instagram"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-facebook"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-youtube"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-twitter"></i></a>
+                                        <a className="brand-icon" href="/"><i className="bi bi-instagram"></i></a>
                                     </li>
                                     <li className="tel">Tel:&nbsp;+(973)&nbsp;17&nbsp;880038</li>
-                                    <li><a className="item" href="#">Chatting service</a></li>
-                                    <li><a className="item" href="#">Submit a Ticket</a></li>
-                                    <li><a className="item" href="#">Our location</a></li>
+                                    <li><a className="item" href="/">Chatting service</a></li>
+                                    <li><a className="item" href="/">Submit a Ticket</a></li>
+                                    <li><a className="item" href="/">Our location</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -136,9 +136,9 @@ const Footer = () => {
                     <div className="row">
                         <div className="col-lg-6 col-12">
                             <ul>
-                                <li><a className="link pos-1" href="#">Sitemap</a></li>
-                                <li><a className="link pos-2" href="#">Terms of Service</a></li>
-                                <li><a className="link pos-3" href="#">Privacy Policy</a></li>
+                                <li><a className="link pos-1" href="/">Sitemap</a></li>
+                                <li><a className="link pos-2" href="/">Terms of Service</a></li>
+                                <li><a className="link pos-3" href="/">Privacy Policy</a></li>
                             </ul>
                         </div>
                         <div className="col-lg-6 col-12 text d-flex justify-content-end">&copy; Copyright 2022 Mablenet Hosting LLC, All Rights Reserved</div>
